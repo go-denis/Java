b13b07872c8688561a2f2e02139e745cd99fc01d
Delfin-Go <go-denis@mail.ru>
2023-03-07 00:21:27 +0300

    Готово

--- Years/src/main/java/Years/Main.java
+++ Years/src/main/java/Years/Main.java
@@ -1,7 +1,27 @@
 package Years;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Введите год 'Например: 2020': ");
+        int year = sc.nextInt();
+
+        checkYear(year);
 
     }
+
+    private static void checkYear(Integer year) {
+
+        if (!(year % 4 == 0) || ((year % 100 == 0) && !(year % 400 == 0)))
+            System.out.println(year + "год. не високосный");
+        else{
+            System.out.println(year + "год. високосный");
+        }
+
+    }
+
+
 }
